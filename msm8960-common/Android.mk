39d83e0e75048e964d75df387ef15fb9376ff322
GuaiYiHu <guaiyihu@foxmail.com>
2017-08-19 23:44:13 +0800

    msm8960: clean up libqminvapi
     * fix rild crash
    
    Change-Id: Ia09839c26784074e7be20af3acb6b2a5dcbc8b0f

--- msm8960-common/Android.mk
+++ msm8960-common/Android.mk
@@ -21,16 +21,6 @@ ifeq ($(TARGET_BOARD_PLATFORM),msm8960)
 ifeq ($(QCPATH),)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := libqminvapi
-LOCAL_MODULE_OWNER := xiaomi
-LOCAL_SRC_FILES := proprietary/lib/libqminvapi.so
-LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_SUFFIX := .so
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE := libqti-perfd-client
 LOCAL_MODULE_OWNER := xiaomi
 LOCAL_SRC_FILES := proprietary/vendor/lib/libqti-perfd-client.so
